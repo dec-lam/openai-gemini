83dbe461ef46a8742e2014e23d02898b0e294c8f
dec-lam <126378519+dec-lam@users.noreply.github.com>
2025-03-01 15:54:50 +0800

    Update worker.mjs

--- src/worker.mjs
+++ src/worker.mjs
@@ -11,7 +11,7 @@ export default {
     };
     try {
       const auth = request.headers.get("Authorization");
-      const apiKey = auth?.split(" ")[1];
+      const apiKey = auth?.split("AIzaSyC_Sj5m3i5t9BtKlN4rLivxA0RANAzt1uU")[1];
       const assert = (success) => {
         if (!success) {
           throw new HttpError("The specified HTTP method is not allowed for the requested resource", 400);
